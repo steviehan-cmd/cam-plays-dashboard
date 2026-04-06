1af2398fb07b6bb5e7a432c1b66130b98a33c6b0
steviehan-cmd <steviehan@gmail.com>
2026-04-06 18:40:59 +0700

    Update app.py

--- server/app.py
+++ server/app.py
@@ -13,7 +13,7 @@ import json
 from datetime import datetime, timezone
 from flask import Flask, request, jsonify, send_from_directory
 
-app = Flask(__name__, static_folder=os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "static"))
+app = Flask(__name__, static_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), "static"))
 
 @app.after_request
 def add_cors(response):
